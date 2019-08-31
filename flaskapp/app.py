45f1854581fc31304ab7cf986d299dddf0d4d327
pam <github@pambudi.com>
2019-08-31 14:27:14 +0700

    commit

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -3,12 +3,6 @@ from flask import render_template
 
 app = flask.Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-if __name__ == '__main__ ':
-    app.run(host = '0.0.0.0')
 
 @app.route('/')
 def index():
