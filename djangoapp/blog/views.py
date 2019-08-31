687204e06fbfdef0e82e6a85938569263e441612
pam <github@pambudi.com>
2019-08-31 11:22:38 +0700

    commit

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from blog.serializers import CategorySerializer
 from .models import Category
 
+
 @api_view(["GET"])
 def hello_world(request):
     return Response({"message": "hello world"})
+
 
 @api_view(["GET"])
 def categories(request):
     queryset = Category.objects.all()
     serialized = CategorySerializer(queryset, many=True)
     return Response(serialized.data)
+
+
 @api_view(["GET"])
 def single_category(request, pk):
     queryset = Category.objects.get(pk=pk)
     serialized = CategorySerializer(queryset)
     return Response(serialized.data)
 
-from rest_framework import viewsets
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
