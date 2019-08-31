25d5dedc5243cc4c8a6cd011ad01593023588850
pam <github@pambudi.com>
2019-08-31 11:01:56 +0700

    commit

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from blog.views import hello_world
-from blog.views import categories
+from blog.views import categories, single_category
 
 urlpatterns = [
     path('admin/', admin.site.urls),
