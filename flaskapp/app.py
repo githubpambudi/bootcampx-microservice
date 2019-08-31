fe1d7c14a529731b8071354078d94cd2d12a0c04
pam <github@pambudi.com>
2019-08-31 15:48:27 +0700

    commit

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -12,6 +12,7 @@ DRF_PORT = os.getenv('DRF_PORT', 5000)
 
 ROOT_DRF = f"http://{DRF_HOST}:{DRF_PORT}"
 
+
 @app.route('/')
 def index():
     categories = requests.get(f'{ROOT_DRF}/api/v2/categories/')
