ff10b1a798b1ab8f71fac78dd6c4bd41a0820348
pam <github@pambudi.com>
2019-08-31 14:42:17 +0700

    commit

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -3,8 +3,16 @@ from flask import render_template
 
 app = flask.Flask(__name__)
 
+import requests
+
+DRF_HOST = os.getenv('DRF_HOST', 'localhost')
+DRF_PORT = os.getenv('DRF_PORT', 5000)
+
+ROOT_DRF = f"http://{DRF_HOST}:{DRF_PORT}"
 
 @app.route('/')
 def index():
-    menu = ['home', 'news']
-    return render_template('index.html', menu=menu)
+    categories = requests.get(f'{ROOT_DRF}/api/categories/')
+    categories = categories.json()
+
+    return render_template('index.html', categories=categories)
