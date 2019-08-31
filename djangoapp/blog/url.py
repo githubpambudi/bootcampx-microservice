f453a8204016c5a83fa60446ff67b4c0e3cd2581
pam <github@pambudi.com>
2019-08-31 11:17:48 +0700

    commit

--- djangoapp/blog/url.py
+++ djangoapp/blog/url.py
@@ -1,6 +1,9 @@
 from rest_framework import routers
 
+from rest_framework import routers
+
 from blog.views import CategoryViewSet
 
 router = routers.DefaultRouter()
 router.register('categories', CategoryViewSet)
+urlpatterns = router.urls
